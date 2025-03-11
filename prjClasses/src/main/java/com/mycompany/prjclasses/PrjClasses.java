2432c06ad7e351538ea13009ab8f39aad020688a
Biggs27 <calebbiggs06@gmail.com>
2025-03-11 10:42:42 +0200

    Add new class
    working on classes

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -9,8 +9,13 @@ package com.mycompany.prjclasses;
  * @author lab_services_student
  */
 public class PrjClasses {
+     public static String strSecondClassVar = "Testing";
 
     public static void main(String[] args) {
-       System.out.println("Heloo world i just did GitHub");
+        System.out.println("Hello from the 2nd class");
+        
+        newClass nc = new newClass();//creating a object of new class
+        
+        nc.helloClass();//have to use nc to communicate with the class
     }
 }
---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/newClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class newClass {
+    public static String strSecondClassVar = "Testing";
+ 
+    public void helloClass()
+            {
+       
+        
+        
+    }
+}
